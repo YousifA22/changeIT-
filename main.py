ad20fe8986ff8d51aa4af3a0e58c1845d226b8db
Yousif Al sendi <yousifalsendi@cmail.carleton.ca>
2024-11-19 20:27:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -139,6 +139,12 @@ def process_resume():
         if os.path.exists(temp_path):
             os.remove(temp_path)
 
+
+
+@app.route('/')
+def home():
+    return "Welcome to the Resume Analyzer API"
+
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
