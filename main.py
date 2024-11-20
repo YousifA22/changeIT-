ec8a0edca0db0b33563e6d51288dcff83f22bf20
Yousif Al sendi <yousifalsendi@cmail.carleton.ca>
2024-11-19 20:18:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -136,4 +136,5 @@ def process_resume():
             os.remove(temp_path)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
